3fc1d1f77cb89d3c0acc7722d33811130fa7b506
Sergey Borovkov <sergeyborovkov364@gmail.com>
2024-12-31 12:10:05 +0100

    [1] init commit

---
+++ DeliveryEats/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DeliveryEats
+//
+//  Created by Sergey Borovkov on 31.12.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
